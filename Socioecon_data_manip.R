ba037b0c9630b3c4265effba781a43bfeeed89b5
Matt Nuttall <mattnuttall00@gmail.com>
2018-10-09 15:15:59 +0100

    update data_manip
    Identified problem - the loop can't separate the communes that are in the same province and have the same name

--- Socioecon_data_manip.R
+++ Socioecon_data_manip.R
@@ -1,14 +1,16 @@
-setwd("/Users/Matt&Kez/Box Sync/Objective 1/Data/Text files")      # You'll need to change this
+
 library('robustbase')
 library('xlsx')
+library('rJava')
 install.packages('rJava')
+
 dat <- read.csv("Socioeconomic_variables.csv")
 commDat <- read.csv("commGIS.csv")
 
 # Subset indigenous group columns
 dat.ig <- dat[,10:73]
 
-# This new dataframe has 64 columns, representing 64/4=16 groups
+# This new dataframe has 64 columns, representing 64/4 = 16 groups
 # Groups start at columns 1,5,9,13 etc, so we first need to create a sequence of numbers representing this
 grp.start <- seq(from=1,to=61,by=4)
 
@@ -36,18 +38,31 @@ dat2 <- dat2[,-c(10:73)]
 ## add commGIS codes
 
 dat2$CommCode <- NA   # Create an empty column in dat2 to store codes
+lgth <- vector()
 
 for (i in 1:nrow(dat2)){   # For each row in dat2
-  cm <- dat2$Commune[i]    # What is the commune name?
-  pv <- dat2$Province[i]   # What is the province name?
+  cm <- as.character(dat2$Commune[i])    # What is the commune name?
+  pv <- as.character(dat2$Province[i])   # What is the province name?
   cc <- subset(commDat,province==pv & commune==cm)  # Subset the line in commDat that matches these two values
-  dat2$CommCode[i] <- cc[,1]  # Insert the associated commune code into dat2
+  #lgth[i] <- length(cc[,1])
+  if (length(cc[,1])>1){
+    dat2$CommCode[i] <- NA
+  }
+  else{
+    dat2$CommCode[i] <- cc[,1]  # Insert the associated commune code into dat2
+  }
 }
+
+dt <- data.frame(PV=dat2[which(lgth>1),'Province'],CM=dat2[which(lgth>1),'Commune'])
+dt
+
+
 
 ## Edit below code to select by communeGIS not commune
 # To aggregate at the commune level, we can do another loop
 
 commGIS <- unique(dat2$CommCode)    # Unique commune IDs
+commGIS <- commGIS[-which(is.na(commGIS)==T)]
 dat3 <- dat2[1,]        # Create template for final data frame columns
 for (c in 1:length(commGIS)){      # For each commune ID
   sub <- dat2[dat2$CommCode==commGIS[c],]   # Subset the data frame
@@ -58,8 +73,11 @@ for (c in 1:length(commGIS)){      # For each commune ID
     if (cc %in% c(10:13,15,18:28,30:35,37:40,43)){     # If the column number is in this list of numbers...
       dat3[c,cc] <- mean(sub[cc],na.rm=T)                   # Do the column mean
     }
-    if (cc==29){                                                 # If the column number is 29...
-      dat3[c,cc] <- median(sub[cc],na.rm=T)     # Do the median
+    if (cc==29 & length(is.na(sub[cc]))==length(sub[cc])){                              # If the column number is 29...
+        dat3[c,cc] <- NA
+    }
+    if (cc==29 & length(is.na(sub[cc]))!=length(sub[cc])){
+        dat3[c,cc] <- median(sub[cc],na.rm=T)     # Do the median
     } 
   }
 }
