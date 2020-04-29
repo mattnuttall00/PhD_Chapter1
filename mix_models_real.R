c0df3eb3cb45870813a09812e133ab5b53ee7461
Matt Nuttall <mattnuttall00@gmail.com>
2020-04-29 14:52:27 +0100

    tot_pop done, prop_ind ongoing

--- mix_models_real.R
+++ mix_models_real.R
@@ -47,6 +47,10 @@ ggplot(dat, aes(x=tot_pop, y=ForPix))+
   facet_wrap(dat$year, nrow=2)
 # no obvious changes over time
 
+# all years but coloured by year
+ggplot(dat, aes(x=tot_pop, y=ForPix, group=year, colour=year))+
+  geom_point()
+
 # simple linear model
 lm.tot_pop <- lm(ForPix ~ tot_pop, data=dat)
 summary(lm.tot_pop)
@@ -65,6 +69,7 @@ visreg(lm.tot_pop_year, xvar = "tot_pop", by = "year")
 # try glm with poisson structure
 glm.tot_pop_year <- glm(ForPix ~ tot_pop + year, data=dat, family = poisson)
 summary(glm.tot_pop_year)
+# All intercepts for the different factor levels are different from level 1 (2007), but by very small amounts. 2010 has a very small difference and relatively larger SE.
 
 # predict
 newpopdat <- expand.grid(tot_pop = seq(279,39117,length.out = 100),
@@ -83,3 +88,64 @@ ggplot(NULL, aes(x=tot_pop, y=ForPix))+
   geom_line(data=newpopdat, aes(group=year, colour=year), size=1)
 
 
+# check model with interaction
+glm.tot_pop_year_int <- glm(ForPix ~ tot_pop * year, data=dat, family=poisson)
+summary(glm.tot_pop_year_int)
+
+anova(glm.tot_pop_year_int, glm.tot_pop_year, test="Chisq")
+# model with interaction is better
+
+# plot marginal effects
+plot_model(glm.tot_pop_year_int, type="pred", terms = c("tot_pop","year"))
+# Interesting. The shape is the same for all years - the lower the population of the communes, the more forest there is. However, in earlier years communes with lower populations have more forest than communes with an equivalent population in later years (i.e forest is being lost overall, over time).  But in earlier years (07,09,10), the amount of forest drops more quickly with increasing population size. When the population of a commune reaches >4000, the commune is more likely to have more forest in 2011 and 2012 than in earlier years (ie. the lines cross). 
+
+
+## take home message here is that communes with low total populations tend to have more forest. The decrease in forest with increasing population is steep in communes with low populations, but once you get to population sizes of around 10,000, the slope gets less steep. Once you get to a population size of 20,000 you have very little forest, and the slope flattens right out. Interestingly year does seem to make a differnce, although not a huge difference. Could this interaction be showing reforestation / plantation expansion in later years?
+
+    # prop_ind ####
+
+# plot prop_ind against ForPix across all years
+ggplot(dat, aes(x=prop_ind, y=ForPix))+
+  geom_point()
+# this doesn't suggest any obvious trend.  I was expecting communes with more indigneous people to have more forest, but this isn't immediately obvious
+
+# split by year
+ggplot(dat, aes(x=prop_ind, y=ForPix))+
+  geom_point()+
+  facet_wrap(dat$year, nrow=2)
+# the only thing I can spot that changes over time (which may be interesting), is that the number of communes with prop_ind == 1 decreases year on year(far right of each plot). This is showing the ethinc make-up of communes changin, as more in-migrants move into once remote communities. It's not obvious from these plots whether or not the amount of forest in those communes is also decreasing
+
+# fit simple model
+glm.prop_ind <- glm(ForPix ~ prop_ind, data=dat, family=poisson)
+summary(glm.prop_ind)
+# ok so there is a positive relationship between prop_ind and forest cover, which is what I suspected it would be. 
+
+# create new data for plotting
+newinddat <- data.frame(prop_ind = seq(1,0,length.out = 100))
+
+# predict
+glm.prop_ind.pred <- predict(glm.prop_ind, newdata = newinddat, type="response", se=T)
+newinddat <- cbind(newinddat, glm.prop_ind.pred)
+newinddat <- newinddat %>% rename(ForPix = fit)
+
+# plot the fit by iteslf
+ggplot(newinddat, aes(x=prop_ind, y=ForPix))+
+  geom_line()
+
+# plot the fit with the points
+ggplot(NULL, aes(x=prop_ind, y=ForPix))+
+  geom_point(data=dat)+
+  geom_line(data=newinddat, size=1)
+
+
+# now check model with year as an interaction term
+glm.prop_ind_year <- glm(ForPix ~ prop_ind * year, data=dat, family=poisson)
+summary(glm.prop_ind_year)
+
+
+# predict from the model with year for plotting
+newinddat <- expand.grid(prop_ind = seq(0, 1, length.out = 100),
+                         year = c("2007","2008","2009","2010","2011","2012"))  
+glm.tot_pop_year.predict <- predict(glm.tot_pop_year, newdata = newpopdat, type="response", se=T)
+newpopdat <- cbind(newpopdat,glm.tot_pop_year.predict)
+newpopdat <- newpopdat %>% rename(ForPix = fit)
