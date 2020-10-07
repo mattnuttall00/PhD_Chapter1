85f0a045ebde28fe517c7062b7be6d0ea67bc71d
Matt Nuttall <mattnuttall00@gmail.com>
2020-10-07 14:21:30 +0100

    Province function working and plotted

--- mix_models_real.R
+++ mix_models_real.R
@@ -3346,7 +3346,8 @@ ggplot(m1.popden.newdat2, aes(x=pop_den, y=pred))+
 
 # in order to get a provincial "mean" I am going to do the following: predict for each commune within a given province, and then take the mean of those predictions to form the provincial mean. I can then use the commune predictions to show CIs or the "variation" around the mean 
 
-ProvMean.popden <- function(dat,province){
+# this function spits out a dataframe with a range of pop_den values (length=100), the mean prediction for the province, the province name, and the 2.5 and 97.5 quantiles around the mean
+ProvMean.popden <- function(dat=dat1,province){
   
   # extract list of communes 
   communes <- unique(dat$Provcomm[dat$Province==province])
@@ -3400,117 +3401,47 @@ return(mean.df)
   
 }
  
-Stung_Treng <- ProvMean.popden(dat1,"Stung Treng")
 
+## now use the function to get the mean effects for all provinces
 
+# create list of province names
+provs <- as.character(unique(dat1$Province))
 
+# initialise list
+output.list <- list()
 
-# extract list of communes 
-communes <- unique(dat1$Provcomm[dat1$Province=="Stung Treng"])
-
-# Initialise empty dataframe
-compred <- data.frame(pop_den = NULL,
-                      pred = NULL,
-                      commune = NULL,
-                      province = NULL)
-
-# loop through list of communes and predict for each one, and attach results into dataframe
-for(i in 1:length(communes)){
-  newdat <- data.frame(pop_den = seq(min(dat1$pop_den[dat1$Province=="Stung Treng"]),
-                                     max(dat1$pop_den[dat1$Province=="Stung Treng"]), length.out = 100), # range in province
-                       prop_ind = mean(dat1$prop_ind[dat1$Province=="Stung Treng"]), # range in province
-                       areaKM = dat1$areaKM[dat1$Provcomm==communes[i]][1],
-                       year = mean(dat1$year[dat1$Province=="Stung Treng"]),
-                       Province = "Stung Treng",
-                       Provcomm = communes[i])
-  newdat$pred <- as.vector(predict(popdem.m1, type="response",newdata=newdat, re.form=~(year|Province/Provcomm)))
+# loop through list of provinces, applying the function to each one
+for(i in 1:length(provs)){
   
-  # pull out values of pop_den and the predictions, and attach commune and province name. 
-  df <- newdat[ ,c("pop_den","pred")]
-  split <- colsplit(newdat$Provcomm, pattern="_", names=c("Province", "Commune"))
-  comname <- split[1,2]
-  provname <- split[1,1]
-  df$commune <- comname 
-  df$province <- provname
-  compred <- rbind(compred,df)
-  
+  df <- ProvMean.popden(province=provs[i])
+  output.list[[i]] <- df
 }
 
-# get the mean prediction for the province (i.e. mean of all communes for a given value of pop_den)  
-mean.df <- compred %>% group_by(pop_den) %>% summarise_at(vars(pred),mean) %>% 
-  mutate(Province = "Stung Treng")
+# name list elements
+provname <- sub(" ","_", provs)
+names(output.list) <- provname
 
-# get the 2.5 and 97.5 quantiles
-compred_wide <- pivot_wider(compred, names_from = commune, values_from = pred) 
-lnth <- ncol(compred_wide)
-quants <- data.frame(apply(compred_wide[ ,3:lnth], 1, quantile, probs=c(0.025,0.975)))
+# extract list elements
+list2env(output.list, globalenv())
 
-quants.vec <- data.frame(pop_den = compred_wide$pop_den,
-                         Q2.5 = as.numeric(quants[1,]),
-                         Q97.5 = as.numeric(quants[2,]))
-
-# join together
-mean.df <- left_join(mean.df, quants.vec, by="pop_den")
+# rbind
+popden_allprovs <- rbind(Banteay_Meanchey,Battambang,Kampong_Cham,Kampong_Chhnang,Kampong_Speu,Kampong_Thom,
+                         Kampot,Kandal,Koh_Kong,Kracheh,Mondul_Kiri,Phnom_Penh,
+                         Preah_Vihear,Prey_Veng,Pursat,Ratanak_Kiri,Siem_Reap,Preah_Sihanouk,
+                         Stung_Treng,Svay_Rieng,Takeo,Otdar_Meanchey,Kep,Pailin)
 
 
-
-
-
-
-
-
-
-
-
-df.names <- c("mean.df","compred")
-names(Stung_Treng) <- df.names
-list2env(Stung_Treng, globalenv())                
-
-compred_wide <- pivot_wider(compred, names_from = commune, values_from = pred) 
-quants <- data.frame(apply(compred_wide[ ,3:36], 1, quantile, probs=c(0.025,0.975)))
-
-quants.vec <- data.frame(pop_den = compred_wide$pop_den,
-                         Q2.5 = as.numeric(quants[1,]),
-                         Q97.5 = as.numeric(quants[2,]))
-
-mean.df <- left_join(mean.df, quants.vec, by="pop_den")
-
-ggplot(mean.df, aes(x=pop_den, y=pred))+
+ggplot(popden_allprovs, aes(x=pop_den, y=pred, group=Province))+
   geom_line()+
   geom_ribbon(aes(ymin=Q2.5, ymax=Q97.5),fill="grey60", alpha=0.3)+
-  theme(panel.background = element_blank(),axis.line = element_line(colour = "grey20"))
+  theme(panel.background = element_blank(),axis.line = element_line(colour = "grey20"))+
+  facet_wrap(~Province, nrow=6, scales = "free")
 
 
 
 
 
-# create vector of provinces
-provinces <- unique(dat1$Province)
 
-# create dataframe of provinces with their mean areaKM value (i.e. mean commune area)
-areas <- dat1 %>% group_by(Province) %>% 
-          mutate(meanArea = mean(areaKM)) %>%
-          select(Province,meanArea) %>% 
-          distinct(Province, .keep_all=TRUE)
-
-# find 1 commune per province with the intercept closest to 0
-m1.ranef <- ranef(popdem.m1)$"Provcomm:Province"
-m1.ranef <- m1.ranef %>% rownames_to_column("Provcomm:Province") 
-provsplit <- as.vector(strsplit(m1.ranef$`Provcomm:Province`, ":"))
-m1.ranef$Province <- strsplit(m1.ranef$`Provcomm:Province`, ":")
-
-
-# create newdata with all provinces
-m1.popdem.newdat.prov <- expand.grid(pop_den = seq(min(dat1$pop_den),max(dat1$pop_den), length.out = 100),
-                                     prop_ind = mean(dat1$prop_ind),
-                                     year = mean(dat1$year),
-                                     Province = provinces)
-
-# add mean areas
-m1.popdem.newdat.prov$areaKM <- areas$meanArea[match(m1.popdem.newdat.prov$Province,areas$Province)]
-
-# predict
-m1.popdem.newdat.prov$pred <- as.vector(predict(popdem.m1, type="response", re.form=~(year|Province)))
 
 
 
