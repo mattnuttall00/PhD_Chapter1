380a7df01a71d983411e9e715686f8ff52c7b30a
Matt Nuttall <mattnuttall00@gmail.com>
2020-11-04 10:43:17 +0000

    Prov & PA grp effects run for econ & access

--- mix_models_real.R
+++ mix_models_real.R
@@ -4612,6 +4612,76 @@ ggplot(pa_all, aes(x=Les1_R_Land, y=pred, group=PA, colour=PA, fill=PA))+
 
 # pig_fam
 
+PAmean.econ <- function(dat=dat1,pa){
+  
+  # extract list of communes 
+  communes <- unique(dat$Provcomm[dat$PA==pa])
+  
+  # Initialise empty dataframe
+  compred <- data.frame(pig_fam = NULL,
+                        pred = NULL,
+                        commune = NULL,
+                        PA = NULL)
+  
+  # loop through list of communes and predict for each one, and attach results into dataframe
+  for(i in 1:length(communes)){
+    newdat <- data.frame(pig_fam = seq(min(dat$pig_fam[dat$PA==pa]),
+                                       max(dat$pig_fam[dat$PA==pa]), length.out = 100), # range in province
+                         Les1_R_Land =  mean(dat$Les1_R_Land[dat$PA==pa]),
+                         areaKM = dat$areaKM[dat$Provcomm==communes[i]][1],
+                         year = mean(dat$year[dat$PA==pa]),
+                         Province = dat$Province[dat$Provcomm==communes[i]][1],
+                         Provcomm = communes[i])
+    newdat$pred <- as.vector(predict(econ.m1, type="response",newdata=newdat, re.form=~(year|Province/Provcomm)))
+    
+    # pull out values of pig_fam and the predictions, and attach commune name and PA status. 
+    df <- newdat[ ,c("pig_fam","pred")]
+    split <- colsplit(newdat$Provcomm, pattern="_", names=c("Province", "Commune"))
+    comname <- split[1,2]
+    df$commune <- comname 
+    df$PA <- pa
+    compred <- rbind(compred,df)
+    
+    
+  }
+  
+  # get the mean prediction for the province (i.e. mean of all communes for a given value of pig_fam)  
+  mean.df <- compred %>% group_by(pig_fam) %>% summarise_at(vars(pred),mean) %>% 
+    mutate(PA = pa)
+  
+  # get the 2.5 and 97.5 quantiles. I have to create unique identifier from the row names first, because there are duplicate rows in the data so pivot_wider gets grumpy and spits out something weird
+  compred_wide <- compred %>% 
+                  pivot_wider(., names_from = commune, values_from = pred, values_fn = list(pred=mean))  
+  lnth <- ncol(compred_wide)
+  quants <- data.frame(apply(compred_wide[ ,3:lnth], 1, quantile, probs=c(0.025,0.975)))
+  
+  quants.vec <- data.frame(pig_fam = compred_wide$pig_fam,
+                           Q2.5 = as.numeric(quants[1,]),
+                           Q97.5 = as.numeric(quants[2,]))
+  
+  # join together
+  mean.df <- left_join(mean.df, quants.vec, by="pig_fam")
+  
+  return(mean.df)
+  
+}
+
+# run function for communes with PAs
+pa_mean <- PAmean.econ(pa="1")
+
+# run function for communes with no PAs. Remove PP for the noPA group
+nopa_mean <- PAmean.econ(dat=dat1[dat1$Province!="Phnom Penh",], pa="0")
+pa_all <- rbind(pa_mean,nopa_mean)
+
+# plot
+ggplot(pa_all, aes(x=pig_fam, y=pred, group=PA, colour=PA, fill=PA))+
+  geom_line(size=1)+
+  geom_ribbon(aes(ymin=Q2.5, ymax=Q97.5), alpha=0.3, colour=NA)+
+  theme(panel.background = element_blank(),axis.line = element_line(colour = "grey20"))
+  #xlim(-0.15,0.2)
+  #ylim(0,7500)
+# no effect at all for either group
+
 
   ## Access to services ####
 
@@ -4669,7 +4739,7 @@ acc.AIC <- arrange(acc.AIC,dAICc)
 
 #
     ## ALL COMMUNES ####
-      # acc.m1 ####
+      # acc.m1, acc.m2, acc.m3 ####
 
 acc.m1 <- glmer(ForPix ~ dist_sch + garbage + KM_Comm + offset(log(areaKM)) + (year|Province/Provcomm),
                 family="poisson", data=dat1)
@@ -4687,7 +4757,232 @@ acc.m2 <- glmer(ForPix ~ dist_sch + garbage + offset(log(areaKM)) + (year|Provin
 summary(acc.m2)
 # no change in effect size
 
-# I will take two strongest effects forward - dist_sch and garbage
+# anova
+anova(acc.m1,acc.m2)
+# simpler model is better
+
+# remove next smallest effect (dist_sch)
+acc.m3 <- glmer(ForPix ~ garbage + offset(log(areaKM)) + (year|Province/Provcomm),
+                family="poisson", data=dat1)
+
+summary(acc.m3)
+
+# anova
+anova(acc.m2,acc.m3)
+
+# the simpler model is the best for use in the Province/Pa no PA analysis.
+
+
+# I will take two strongest effects forward for the global model - dist_sch and garbage
+
+
+      # garbage effects between provinces ####
+
+# in order to get a provincial "mean" I am going to do the following: predict for each commune within a given province, and then take the mean of those predictions to form the provincial mean. I can then use the commune predictions to show CIs or the "variation" around the mean 
+
+
+### garbage
+
+# this function spits out a dataframe with a range of garbage values (length=100), the mean prediction for the province, the province name, and the 2.5 and 97.5 quantiles around the mean
+ProvMean.acc <- function(dat=dat1,province, model){
+  
+  # extract list of communes 
+  communes <- unique(dat$Provcomm[dat$Province==province])
+  
+  # Initialise empty dataframe
+  compred <- data.frame(garbage = NULL,
+                        pred = NULL,
+                        commune = NULL,
+                        province = NULL)
+  
+  # loop through list of communes and predict for each one, and attach results into dataframe
+  for(i in 1:length(communes)){
+    newdat <- data.frame(garbage = seq(min(dat$garbage[dat$Province==province]),
+                                       max(dat$garbage[dat$Province==province]), length.out = 100), 
+                         areaKM = dat$areaKM[dat$Provcomm==communes[i]][1],
+                         year = mean(dat$year[dat$Province==province]),
+                         Province = province,
+                         Provcomm = communes[i])
+    newdat$pred <- as.vector(predict(model, type="response",newdata=newdat, re.form=~(year|Province/Provcomm)))
+    
+    # pull out values of garbage and the predictions, and attach commune and province name. 
+    df <- newdat[ ,c("garbage","pred")]
+    split <- colsplit(newdat$Provcomm, pattern="_", names=c("Province", "Commune"))
+    comname <- split[1,2]
+    provname <- split[1,1]
+    df$commune <- comname 
+    df$province <- provname
+    compred <- rbind(compred,df)
+    
+    
+  }
+  
+# get the mean prediction for the province (i.e. mean of all communes for a given value of pop_den)  
+mean.df <- compred %>% group_by(garbage) %>% summarise_at(vars(pred),mean) %>% 
+            mutate(Province = province)
+
+# get the 2.5 and 97.5 quantiles
+compred_wide <- pivot_wider(compred, names_from = commune, values_from = pred) 
+lnth <- ncol(compred_wide)
+quants <- data.frame(apply(compred_wide[ ,3:lnth], 1, quantile, probs=c(0.025,0.975)))
+
+quants.vec <- data.frame(garbage = compred_wide$garbage,
+                         Q2.5 = as.numeric(quants[1,]),
+                         Q97.5 = as.numeric(quants[2,]))
+
+# join together
+mean.df <- left_join(mean.df, quants.vec, by="garbage")
+
+return(mean.df)
+  
+}
+ 
+test <- ProvMean.acc(dat1,"Stung Treng",acc.m3)
+
+
+## now use the function to get the mean effects for all provinces
+
+# create list of province names
+provs <- as.character(unique(dat1$Province))
+
+# initialise list
+output.list <- list()
+
+# loop through list of provinces, applying the function to each one
+for(i in 1:length(provs)){
+  
+  df <- ProvMean.acc(province=provs[i], model=acc.m3)
+  output.list[[i]] <- df
+}
+
+
+
+# name list elements
+provname <- sub(" ","_", provs)
+names(output.list) <- provname
+
+# extract list elements
+list2env(output.list, globalenv())
+
+# rbind
+acc_allprovs <- rbind(Banteay_Meanchey,Battambang,Kampong_Cham,Kampong_Chhnang,Kampong_Speu,Kampong_Thom,
+                         Kampot,Kandal,Koh_Kong,Kracheh,Mondul_Kiri,Phnom_Penh,
+                         Preah_Vihear,Prey_Veng,Pursat,Ratanak_Kiri,Siem_Reap,Preah_Sihanouk,
+                         Stung_Treng,Svay_Rieng,Takeo,Otdar_Meanchey,Kep,Pailin)
+
+# plot with PP and no free axis
+ggplot(acc_allprovs, aes(x=garbage, y=pred, group=Province))+
+  geom_line()+
+  geom_ribbon(aes(ymin=Q2.5, ymax=Q97.5),fill="grey60", alpha=0.3)+
+  theme(panel.background = element_blank(),axis.line = element_line(colour = "grey20"))+
+  facet_wrap(~Province, nrow=6, scales = "free")
+
+
+# remove PP and no free axis
+ggplot(acc_allprovs[acc_allprovs$Province!="Phnom Penh",], aes(x=garbage, y=pred, group=Province))+
+  geom_line()+
+  geom_ribbon(aes(ymin=Q2.5, ymax=Q97.5),fill="grey60", alpha=0.3)+
+  theme(panel.background = element_blank(),axis.line = element_line(colour = "grey20"))+
+  facet_wrap(~Province, nrow=6)
+# This plot shows that there is no effect at the province level
+
+# remove PP and free axis
+ggplot(acc_allprovs[acc_allprovs$Province!="Phnom Penh",], aes(x=garbage, y=pred, group=Province))+
+  geom_line()+
+  geom_ribbon(aes(ymin=Q2.5, ymax=Q97.5),fill="grey60", alpha=0.3)+
+  theme(panel.background = element_blank(),axis.line = element_line(colour = "grey20"))+
+  facet_wrap(~Province, nrow=6, scales = "free")
+# Yep - no effect at all 
+
+
+
+
+
+
+      # garbage effects between PA and non_PA communes ####
+
+## now I want to have a look at differences between groups of communes. Specifically I want to look at communes in and around PAs. This is to see if there is a difference in effect of garbage on the more rural/remote areas with high forest cover and PAs. 
+
+# I need to do the same as above - split the communes into groups, predict for each commune within a group, and then get the mean, using the quantiles to show variation.  I can adapt my function from above
+
+# I can use the PA variable in dat1 to split the groups
+
+
+### garbage
+
+PAmean.acc <- function(dat=dat1,pa){
+  
+  # extract list of communes 
+  communes <- unique(dat$Provcomm[dat$PA==pa])
+  
+  # Initialise empty dataframe
+  compred <- data.frame(garbage = NULL,
+                        pred = NULL,
+                        commune = NULL,
+                        PA = NULL)
+  
+  # loop through list of communes and predict for each one, and attach results into dataframe
+  for(i in 1:length(communes)){
+    newdat <- data.frame(garbage = seq(min(dat$garbage[dat$PA==pa]),
+                                       max(dat$garbage[dat$PA==pa]), length.out = 100), # range in province
+                         areaKM = dat$areaKM[dat$Provcomm==communes[i]][1],
+                         year = mean(dat$year[dat$PA==pa]),
+                         Province = dat$Province[dat$Provcomm==communes[i]][1],
+                         Provcomm = communes[i])
+    newdat$pred <- as.vector(predict(acc.m3, type="response",newdata=newdat, re.form=~(year|Province/Provcomm)))
+    
+    # pull out values of garbage and the predictions, and attach commune name and PA status. 
+    df <- newdat[ ,c("garbage","pred")]
+    split <- colsplit(newdat$Provcomm, pattern="_", names=c("Province", "Commune"))
+    comname <- split[1,2]
+    df$commune <- comname 
+    df$PA <- pa
+    compred <- rbind(compred,df)
+    
+    
+  }
+  
+  # get the mean prediction for the province (i.e. mean of all communes for a given value of garbage)  
+  mean.df <- compred %>% group_by(garbage) %>% summarise_at(vars(pred),mean) %>% 
+    mutate(PA = pa)
+  
+  # get the 2.5 and 97.5 quantiles. I have to create unique identifier from the row names first, because there are duplicate rows in the data so pivot_wider gets grumpy and spits out something weird
+  compred_wide <- compred %>% 
+                  pivot_wider(., names_from = commune, values_from = pred, values_fn = list(pred=mean))  
+  lnth <- ncol(compred_wide)
+  quants <- data.frame(apply(compred_wide[ ,3:lnth], 1, quantile, probs=c(0.025,0.975)))
+  
+  quants.vec <- data.frame(garbage = compred_wide$garbage,
+                           Q2.5 = as.numeric(quants[1,]),
+                           Q97.5 = as.numeric(quants[2,]))
+  
+  # join together
+  mean.df <- left_join(mean.df, quants.vec, by="garbage")
+  
+  return(mean.df)
+  
+}
+
+# run function for communes with PAs
+pa_mean <- PAmean.acc(pa="1")
+
+# run function for communes with no PAs. Remove PP for the noPA group
+nopa_mean <- PAmean.acc(dat=dat1[dat1$Province!="Phnom Penh",], pa="0")
+pa_all <- rbind(pa_mean,nopa_mean)
+
+# plot
+ggplot(pa_all, aes(x=garbage, y=pred, group=PA, colour=PA, fill=PA))+
+  geom_line(size=1)+
+  geom_ribbon(aes(ymin=Q2.5, ymax=Q97.5), alpha=0.3, colour=NA)+
+  theme(panel.background = element_blank(),axis.line = element_line(colour = "grey20"))
+  #xlim(-0.15,0.2)
+  #ylim(0,7500)
+# no effect at all for either group
+
+
+
+
+
 
 
   ## Social justice ####
