9fd47f516b787deb573fae02063a24267034bb0a
Matt Nuttall <mattnuttall00@gmail.com>
2021-05-20 11:35:41 +0100

    Code to run global macroecon models
    Need to run dredge over the weekend (or as a job)

--- macroeconomic_analysis.R
+++ macroeconomic_analysis.R
@@ -4426,3 +4426,54 @@ ggsave("Results/Macroeconomics/Plots/ELCs/prod_elc_all_noPts.png", p.prod.all,
 
   ## Global set ####
 
+# here I am going to try and put all variables from each set, including all lags, into a single model set. I can't put all vars from all sets together because when you include all lagged vars as well there are way more variables than there are data points. 
+
+# load data (pre-made in Excel)
+dat <- read.csv("dat_work_glob.csv", header = TRUE)
+
+str(dat)
+
+# I need to create a new time variable that spans the whole period (1994 - 2017)
+time_mod <- lm(elc ~ year, data=dat)
+time_mod$residuals
+resids <- c(NA,time_mod$residuals)
+dat$time <- resids
+
+# drop gdp_gr because there are too many missing values - it's reducing the size of the data
+dat <- dat %>% select(!c(gdp_gr, gdp_gr.L1, gdp_gr.L2))
+
+
+# drop rows with NAs
+dat <- dat %>% drop_na()
+
+    # Macroecon ####
+
+# full model - gdp, gdp_gr, fdi, ind_gdp, agr_gdp, dev_agri, dev_env, pop_den
+m.macroecon.glob <- glm(elc ~ gdp+gdp.L1+gdp.L2+fdi+fdi.L1+fdi.L2+
+                          ind_gdp+ind_gdp.L1+ind_gdp.L2+agr_gdp+agr_gdp.L1+agr_gdp.L2+
+                          dev_agri+dev_agri.L1+dev_agri.L2+dev_env+dev_env.L1+dev_env.L2+
+                          pop_den+pop_den.L1+pop_den.L2+for_rem+for_rem.L1+for_rem.L2+time,
+                        na.action = "na.fail", family=poisson, data=dat)
+
+# dredge
+m.econ.glob.d <- dredge(m.macroecon.glob, beta = "none", evaluate = TRUE, rank = AICc)
+
+
+
+
+
+    # Commodity ####
+
+# full model - cpi + nfi + rice_med + rub_med + corn_med + sug_med + for_prod + time + for_rem
+m.comm.glob <- glm(elc ~ cpi + cpi.L1 + cpi.L2 +
+                     nfi + nfi.L1 + nfi.L2 +
+                     rice_med + rice_med.L1 + rice_med.L2 +
+                     rub_med + rub_med.L1 + rub_med.L2 +
+                     corn_med + corn_med.L1 + corn_med.L2 +
+                     sug_med + sug_med.L1 + sug_med.L2 +
+                     for_prod + for_prod.L1 + for_prod.L2 +
+                     for_rem + for_rem.L1 + for_rem.L2 +
+                     time,
+                   na.action = "na.fail", family=poisson, data=dat)
+
+
