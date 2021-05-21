a5df847f1acbe9506b880695d9602295a8e6f061
Matt Nuttall <mattnuttall00@gmail.com>
2021-05-21 11:47:29 +0100

    Changed model for dredging - no longer doing this
    This was the job script to run a dredge on all macroecon vars (all lags). I have decided not to do this because a) it takes too long to run (R crashed ovenight), and b) I am not sure whether putting different lags into the same model is correct

--- Jobs/macroecon_elc_glob.R
+++ Jobs/macroecon_elc_glob.R
@@ -17,11 +17,12 @@ dat <- dat %>% select(!c(gdp_gr, gdp_gr.L1, gdp_gr.L2))
 dat <- dat %>% drop_na()
 
 # full model - gdp, gdp_gr, fdi, ind_gdp, agr_gdp, dev_agri, dev_env, pop_den
-m.macroecon.glob <- glm(elc ~ gdp+gdp.L1+gdp.L2+fdi+fdi.L1+fdi.L2+
-                          ind_gdp+ind_gdp.L1+ind_gdp.L2+agr_gdp+agr_gdp.L1+agr_gdp.L2+
-                          dev_agri+dev_agri.L1+dev_agri.L2+dev_env+dev_env.L1+dev_env.L2+
-                          pop_den+pop_den.L1+pop_den.L2+for_rem+for_rem.L1+for_rem.L2+time,
+m.macroecon.glob <- glm(elc ~ gdp.L1+gdp.L2+fdi.L1+
+                              agr_gdp+agr_gdp.L1+
+                              dev_env+
+                              pop_den+pop_den.L1+pop_den.L2+for_rem+for_rem.L1+for_rem.L2+time,
                         na.action = "na.fail", family=poisson, data=dat)
+
 
 # dredge
 m.econ.glob.d <- dredge(m.macroecon.glob, beta = "none", evaluate = TRUE, rank = AICc)
